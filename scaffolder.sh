3d7ef37315b9d5cef219d350a1a001778f1e89f3
jbudding <jbudding@ndapm.com>
2014-04-15 15:31:47 -0500

    Added Variables, Protection, index content

--- scaffolder.sh
+++ scaffolder.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 #
-# Always set a list of variables with the names and list of things you want to do or change at
-# the top of your Shell Script.  Your script will quickly get so long you will not be able
-# to see where in your script you need to change when it gets too long. - @jbudding
-$DIRS_TO_CREATE="css scss img scripts design-docs"
+# Always set a list of variables with the names and list of things you want to 
+# do or change at the top of your Shell Script.  Your script will quickly get so
+# long you will not be able to see where in your script you need to change when 
+# it gets too long. - @jbudding
+#
+DIRS_TO_CREATE="css scss img scripts design-docs"
+INDEX_FILE="index.html"
 
-cd ~/Desktop/;
+cd ~/Desktop/
 
-echo "in" `pwd`;
+echo "in" `pwd`
 
-mkdir scaffold; 
+mkdir -p scaffold 
 
-cd ~/Desktop/scaffold;
+cd ~/Desktop/scaffold
 
-echo "in" `pwd`;
+echo "in" `pwd`
 
-touch index.html;
+if [ -f $INDEX_FILE ]
+then
+	echo "File $INDEX_FILE already exists, not creating."
+else
+	touch $INDEX_FILE
+	echo "<HTML>" >> $INDEX_FILE
+	echo "</HTML>" >> $INDEX_FILE
+	
+fi
 
-mkdir $DIRS_TO_CREATE
+for item in $DIRS_TO_CREATE
+do 
+	echo "Creating $item"
+	mkdir -p $item
+done
+
+
