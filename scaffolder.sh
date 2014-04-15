6dd96b85f17c5daed88ca0843c6da0b4d4a287c0
jbudding <jbudding@ndapm.com>
2014-04-15 14:56:10 -0500

    Use Variables for ease of future script mods

--- scaffolder.sh
+++ scaffolder.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Always set a list of variables with the names and list of things you want to do or change at
+# the top of your Shell Script.  Your script will quickly get so long you will not be able
+# to see where in your script you need to change when it gets too long. - @jbudding
+$DIRS_TO_CREATE="css scss img scripts design-docs"
 
 cd ~/Desktop/;
 
@@ -12,4 +17,4 @@ echo "in" `pwd`;
 
 touch index.html;
 
-mkdir css scss img scripts design-docs;
+mkdir $DIRS_TO_CREATE
